084a3cd003f30015bb74e60cff1a792be2e04d81
Nighty3098 <night3098games@gmail.com>
2024-11-25 17:26:32 +0700

    🌿 Production

--- src/log_monitor.c
+++ src/log_monitor.c
@@ -43,252 +43,225 @@ regex_t regex_patterns[8];
 static int running = 1;
 pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void handle_signal(int signal)
-{
-    if (signal == SIGINT)
-    {
-        running = 0;
-        printf(RED "\n\nBye...\n");
-    }
+void handle_signal(int signal) {
+  if (signal == SIGINT) {
+    running = 0;
+    printf(RED "\n\nBye...\n");
+  }
 }
 
-void compile_regex_patterns()
-{
-    const char *patterns[] = {"\\|\\s*CRITICAL\\s*\\|", "\\|\\s*WARNING\\s*\\|", "\\|\\s*INFO\\s*\\|",
-                              "\\|\\s*DEBUG\\s*\\|",    "\\|\\s*ERROR\\s*\\|",   "\\|\\s*UNKNOWN\\s*\\|",
-                              "\\|\\s*TRACE\\s*\\|",    "\\|\\s*FATAL\\s*\\|"};
+void compile_regex_patterns() {
+  const char *patterns[] = {"\\|\\s*CRITICAL\\s*\\|", "\\|\\s*WARNING\\s*\\|",
+                            "\\|\\s*INFO\\s*\\|",     "\\|\\s*DEBUG\\s*\\|",
+                            "\\|\\s*ERROR\\s*\\|",    "\\|\\s*UNKNOWN\\s*\\|",
+                            "\\|\\s*TRACE\\s*\\|",    "\\|\\s*FATAL\\s*\\|"};
 
-    for (int i = 0; i < sizeof(patterns) / sizeof(patterns[0]); i++)
-    {
-        if (regcomp(&regex_patterns[i], patterns[i], REG_EXTENDED | REG_ICASE) != 0)
-        {
-            fprintf(stderr, "Failed to compile regex: %s\n", patterns[i]);
-            exit(EXIT_FAILURE);
-        }
+  for (int i = 0; i < sizeof(patterns) / sizeof(patterns[0]); i++) {
+    if (regcomp(&regex_patterns[i], patterns[i], REG_EXTENDED | REG_ICASE) !=
+        0) {
+      fprintf(stderr, "Failed to compile regex: %s\n", patterns[i]);
+      exit(EXIT_FAILURE);
     }
+  }
 }
 
-void free_regex_patterns()
-{
-    for (int i = 0; i < sizeof(regex_patterns) / sizeof(regex_patterns[0]); i++)
-    {
-        regfree(&regex_patterns[i]);
-    }
+void free_regex_patterns() {
+  for (int i = 0; i < sizeof(regex_patterns) / sizeof(regex_patterns[0]); i++) {
+    regfree(&regex_patterns[i]);
+  }
 }
 
-void count_log_levels(const char *line)
-{
-    pthread_mutex_lock(&count_mutex);
+void count_log_levels(const char *line) {
+  pthread_mutex_lock(&count_mutex);
 
-    int matched = 0;
+  int matched = 0;
 
-    for (int i = 0; i < sizeof(regex_patterns) / sizeof(regex_patterns[0]); i++)
-    {
-        if (regexec(&regex_patterns[i], line, 0, NULL, 0) == 0)
-        {
-            matched = 1;
-            switch (i)
-            {
-            case 0:
-                critical_count++;
-                break; // CRITICAL
-            case 1:
-                warning_count++;
-                break; // WARNING
-            case 2:
-                info_count++;
-                break; // INFO
-            case 3:
-                debug_count++;
-                break; // DEBUG
-            case 4:
-                error_count++;
-                break; // ERROR
-            case 5:
-                unknown_count++;
-                break; // UNKNOWN
-            case 6:
-                trace_count++;
-                break; // TRACE
-            case 7:
-                fatal_count++;
-                break; // FATAL
-            }
-            break;
-        }
-    }
-
-    if (!matched)
-    {
+  for (int i = 0; i < sizeof(regex_patterns) / sizeof(regex_patterns[0]); i++) {
+    if (regexec(&regex_patterns[i], line, 0, NULL, 0) == 0) {
+      matched = 1;
+      switch (i) {
+      case 0:
+        critical_count++;
+        break; // CRITICAL
+      case 1:
+        warning_count++;
+        break; // WARNING
+      case 2:
+        info_count++;
+        break; // INFO
+      case 3:
+        debug_count++;
+        break; // DEBUG
+      case 4:
+        error_count++;
+        break; // ERROR
+      case 5:
         unknown_count++;
+        break; // UNKNOWN
+      case 6:
+        trace_count++;
+        break; // TRACE
+      case 7:
+        fatal_count++;
+        break; // FATAL
+      }
+      break;
     }
+  }
 
-    pthread_mutex_unlock(&count_mutex);
+  if (!matched) {
+    unknown_count++;
+  }
+
+  pthread_mutex_unlock(&count_mutex);
 }
 
-void process_line(const char *line, char *filter_levels[], int filter_count, int print_lines, char *start_date,
-                  char *end_date)
-{
-    if (should_print_log(line, filter_levels, filter_count, start_date, end_date))
-    {
-        if (print_lines)
-        {
-            colorize_log(line);
-            count_log_levels(line);
-        }
-        else
-        {
-            count_log_levels(line);
-        }
+void process_line(const char *line, char *filter_levels[], int filter_count,
+                  int print_lines, char *start_date, char *end_date) {
+  if (should_print_log(line, filter_levels, filter_count, start_date,
+                       end_date)) {
+    if (print_lines) {
+      colorize_log(line);
+      count_log_levels(line);
+    } else {
+      count_log_levels(line);
     }
-    else
-    {
-        count_log_levels(line);
-    }
+  } else {
+    count_log_levels(line);
+  }
 }
 
-void start_log_monitor(const char *file_name, char *filter_levels[], int filter_count, int real_time, int show_stats,
-                       int print_lines, char *start_date, char *end_date)
-{
-    signal(SIGINT, handle_signal);
+void start_log_monitor(const char *file_name, char *filter_levels[],
+                       int filter_count, int real_time, int show_stats,
+                       int print_lines, char *start_date, char *end_date) {
+  signal(SIGINT, handle_signal);
 
-    compile_regex_patterns();
-    if (show_stats)
-    {
-        start_monitoring();
-    }
+  compile_regex_patterns();
+  if (show_stats) {
+    start_monitoring();
+  }
 
-    int fd = open(file_name, O_RDONLY);
-    if (fd == -1)
-    {
-        perror("open");
-        free_regex_patterns();
-        return;
-    }
-
-    char *buffer = malloc(INITIAL_BUFFER_SIZE);
-    size_t buffer_size = INITIAL_BUFFER_SIZE;
-    size_t current_length = 0;
-
-    if (!real_time)
-    {
-        ssize_t bytes_read;
-
-        while ((bytes_read = read(fd, buffer + current_length, buffer_size - current_length - 1)) > 0)
-        {
-            current_length += bytes_read;
-            buffer[current_length] = '\0';
-
-            char *line_start = buffer;
-            char *line_end;
-
-            while ((line_end = strchr(line_start, '\n')) != NULL)
-            {
-                *line_end = '\0';
-                process_line(line_start, filter_levels, filter_count, print_lines, start_date, end_date);
-                line_start = line_end + 1;
-            }
-
-            current_length -= (line_start - buffer);
-            memmove(buffer, line_start, current_length);
-        }
-
-        close(fd);
-        print_file_size(file_name);
-        print_statistics();
-        if (show_stats)
-        {
-            stop_monitoring();
-        }
-        free_regex_patterns();
-        free(buffer);
-        return;
-    }
-
-    int inotify_fd = inotify_init();
-    if (inotify_fd < 0)
-    {
-        perror("inotify_init");
-        close(fd);
-        free(buffer);
-        return;
-    }
-
-    int wd = inotify_add_watch(inotify_fd, file_name, IN_MODIFY);
-    if (wd == -1)
-    {
-        perror("inotify_add_watch");
-        close(fd);
-        close(inotify_fd);
-        free(buffer);
-        return;
-    }
-
-    off_t offset = lseek(fd, 0, SEEK_END);
-
-    while (running)
-    {
-        fd_set readfds;
-        FD_ZERO(&readfds);
-        FD_SET(inotify_fd, &readfds);
-
-        struct timeval tv;
-        tv.tv_sec = 1;
-        tv.tv_usec = 0;
-
-        int retval = select(inotify_fd + 1, &readfds, NULL, NULL, &tv);
-
-        if (retval == -1)
-        {
-            perror("select");
-            break;
-        }
-        else if (retval == 0)
-        {
-            continue;
-        }
-
-        char event_buf[EVENT_BUF_LEN];
-
-        ssize_t bytes = read(inotify_fd, event_buf, EVENT_BUF_LEN);
-
-        if (bytes < 0)
-        {
-            perror("read");
-            break;
-        }
-
-        lseek(fd, offset, SEEK_SET);
-        ssize_t bytes_read = read(fd, buffer + current_length, buffer_size - current_length - 1);
-
-        if (bytes_read > 0)
-        {
-            current_length += bytes_read;
-            buffer[current_length] = '\0';
-
-            char *line_start = buffer;
-            char *line_end;
-
-            while ((line_end = strchr(line_start, '\n')) != NULL)
-            {
-                *line_end = '\0';
-                process_line(line_start, filter_levels, filter_count, print_lines, start_date, end_date);
-                line_start = line_end + 1;
-            }
-
-            current_length -= (line_start - buffer);
-            memmove(buffer, line_start, current_length);
-            offset += bytes_read;
-        }
-    }
-
-    inotify_rm_watch(inotify_fd, wd);
-
-    print_file_size(file_name);
-    print_statistics();
+  int fd = open(file_name, O_RDONLY);
+  if (fd == -1) {
+    perror("open");
     free_regex_patterns();
-    stop_monitoring();
+    return;
+  }
+
+  char *buffer = malloc(INITIAL_BUFFER_SIZE);
+  size_t buffer_size = INITIAL_BUFFER_SIZE;
+  size_t current_length = 0;
+
+  if (!real_time) {
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd, buffer + current_length,
+                              buffer_size - current_length - 1)) > 0) {
+      current_length += bytes_read;
+      buffer[current_length] = '\0';
+
+      char *line_start = buffer;
+      char *line_end;
+
+      while ((line_end = strchr(line_start, '\n')) != NULL) {
+        *line_end = '\0';
+        process_line(line_start, filter_levels, filter_count, print_lines,
+                     start_date, end_date);
+        line_start = line_end + 1;
+      }
+
+      current_length -= (line_start - buffer);
+      memmove(buffer, line_start, current_length);
+    }
 
     close(fd);
+    print_file_size(file_name);
+    print_statistics();
+    if (show_stats) {
+      stop_monitoring();
+    }
+    free_regex_patterns();
     free(buffer);
+    return;
+  }
+
+  int inotify_fd = inotify_init();
+  if (inotify_fd < 0) {
+    perror("inotify_init");
+    close(fd);
+    free(buffer);
+    return;
+  }
+
+  int wd = inotify_add_watch(inotify_fd, file_name, IN_MODIFY);
+  if (wd == -1) {
+    perror("inotify_add_watch");
+    close(fd);
+    close(inotify_fd);
+    free(buffer);
+    return;
+  }
+
+  off_t offset = lseek(fd, 0, SEEK_END);
+
+  while (running) {
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(inotify_fd, &readfds);
+
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+
+    int retval = select(inotify_fd + 1, &readfds, NULL, NULL, &tv);
+
+    if (retval == -1) {
+      perror("select");
+      break;
+    } else if (retval == 0) {
+      continue;
+    }
+
+    char event_buf[EVENT_BUF_LEN];
+
+    ssize_t bytes = read(inotify_fd, event_buf, EVENT_BUF_LEN);
+
+    if (bytes < 0) {
+      perror("read");
+      break;
+    }
+
+    lseek(fd, offset, SEEK_SET);
+    ssize_t bytes_read =
+        read(fd, buffer + current_length, buffer_size - current_length - 1);
+
+    if (bytes_read > 0) {
+      current_length += bytes_read;
+      buffer[current_length] = '\0';
+
+      char *line_start = buffer;
+      char *line_end;
+
+      while ((line_end = strchr(line_start, '\n')) != NULL) {
+        *line_end = '\0';
+        process_line(line_start, filter_levels, filter_count, print_lines,
+                     start_date, end_date);
+        line_start = line_end + 1;
+      }
+
+      current_length -= (line_start - buffer);
+      memmove(buffer, line_start, current_length);
+      offset += bytes_read;
+    }
+  }
+
+  inotify_rm_watch(inotify_fd, wd);
+
+  print_file_size(file_name);
+  print_statistics();
+  free_regex_patterns();
+  stop_monitoring();
+
+  close(fd);
+  free(buffer);
 }
--- src/main.c
+++ src/main.c
@@ -8,122 +8,105 @@
 #define MAX_FILTERS 10
 #define MAX_FILTER_LENGTH 256
 
-void print_usage(const char *program_name)
-{
-    printf("\n");
-    printf("\n\033[0;31m");
-    printf("▄▄▌         ▄▄ • ▪   ▐ ▄ .▄▄ · ▪   ▄▄ •  ▄ .▄▄▄▄▄▄\n");
-    printf("██•  ▪     ▐█ ▀ ▪██ •█▌▐█▐█ ▀. ██ ▐█ ▀ ▪██▪▐█•██  \n");
-    printf("██▪   ▄█▀▄ ▄█ ▀█▄▐█·▐█▐▐▌▄▀▀▀█▄▐█·▄█ ▀█▄██▀▐█ ▐█.▪\n");
-    printf("▐█▌▐▌▐█▌.▐▌▐█▄▪▐█▐█▌██▐█▌▐█▄▪▐█▐█▌▐█▄▪▐███▌▐▀ ▐█▌·\n");
-    printf(".▀▀▀  ▀█▄▀▪·▀▀▀▀ ▀▀▀▀▀ █▪ ▀▀▀▀ ▀▀▀·▀▀▀▀ ▀▀▀ · ▀▀▀ \n");
-    printf("\n");
-    printf("\n\033[1;33m");
-    printf(" 🗲 Usage: %s [-r] [-dp] [-h] [-f <level>] -i <file>\n", program_name);
-    printf("      -r             Display all changes in real time\n");
-    printf("      -f <level>     Level filtering (CRITICAL, WARNING, INFO, "
-           "DEBUG)\n");
-    printf("      -i <file>      Path to log file\n");
-    printf("      -h, --help     Show this help\n");
-    printf("      -dp            Don't print log lines\n");
+void print_usage(const char *program_name) {
+  printf("\n");
+  printf("\n\033[0;31m");
+  printf("▄▄▌         ▄▄ • ▪   ▐ ▄ .▄▄ · ▪   ▄▄ •  ▄ .▄▄▄▄▄▄\n");
+  printf("██•  ▪     ▐█ ▀ ▪██ •█▌▐█▐█ ▀. ██ ▐█ ▀ ▪██▪▐█•██  \n");
+  printf("██▪   ▄█▀▄ ▄█ ▀█▄▐█·▐█▐▐▌▄▀▀▀█▄▐█·▄█ ▀█▄██▀▐█ ▐█.▪\n");
+  printf("▐█▌▐▌▐█▌.▐▌▐█▄▪▐█▐█▌██▐█▌▐█▄▪▐█▐█▌▐█▄▪▐███▌▐▀ ▐█▌·\n");
+  printf(".▀▀▀  ▀█▄▀▪·▀▀▀▀ ▀▀▀▀▀ █▪ ▀▀▀▀ ▀▀▀·▀▀▀▀ ▀▀▀ · ▀▀▀ \n");
+  printf("\n");
+  printf("\n\033[1;33m");
+  printf(" 🗲 Usage: %s [-r] [-dp] [-h] [-f <level>] -i <file>\n",
+         program_name);
+  printf("      -r             Display all changes in real time\n");
+  printf("      -f <level>     Level filtering (CRITICAL, WARNING, INFO, "
+         "DEBUG)\n");
+  printf("      -i <file>      Path to log file\n");
+  printf("      -h, --help     Show this help\n");
+  printf("      -dp            Don't print log lines\n");
 }
 
-char *normalize_date(const char *input)
-{
-    char *normalized = malloc(20 * sizeof(char));
-    if (normalized == NULL)
-    {
-        return NULL;
-    }
-    struct tm tm = {0};
-    if (sscanf(input, "%4d-%2d-%2d %2d:%2d:%2d", &tm.tm_year, &tm.tm_mon, &tm.tm_mday, &tm.tm_hour, &tm.tm_min,
-               &tm.tm_sec) == 6)
-    {
-        tm.tm_year -= 1900;
-        tm.tm_mon -= 1;
-        strftime(normalized, 20, "%Y-%m-%d %H:%M:%S", &tm);
-        return normalized;
-    }
-    free(normalized);
+char *normalize_date(const char *input) {
+  char *normalized = malloc(20 * sizeof(char));
+  if (normalized == NULL) {
     return NULL;
+  }
+  struct tm tm = {0};
+  if (sscanf(input, "%4d-%2d-%2d %2d:%2d:%2d", &tm.tm_year, &tm.tm_mon,
+             &tm.tm_mday, &tm.tm_hour, &tm.tm_min, &tm.tm_sec) == 6) {
+    tm.tm_year -= 1900;
+    tm.tm_mon -= 1;
+    strftime(normalized, 20, "%Y-%m-%d %H:%M:%S", &tm);
+    return normalized;
+  }
+  free(normalized);
+  return NULL;
 }
 
-int main(int argc, char *argv[])
-{
-    const char *file_name = NULL;
-    char *filter_levels[MAX_FILTERS];
-    int filter_count = 0;
-    int real_time = 0;
-    int show_stats = 0;
-    int print_lines = 1;
-    char *start_date = NULL;
-    char *end_date = NULL;
+int main(int argc, char *argv[]) {
+  const char *file_name = NULL;
+  char *filter_levels[MAX_FILTERS];
+  int filter_count = 0;
+  int real_time = 0;
+  int show_stats = 0;
+  int print_lines = 1;
+  char *start_date = NULL;
+  char *end_date = NULL;
 
-    for (int i = 1; i < argc; i++)
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
+      file_name = argv[++i];
+    } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+      print_usage(argv[0]);
+      return EXIT_SUCCESS;
+    } else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
+      if (filter_count < MAX_FILTERS) {
+        filter_levels[filter_count++] = argv[++i];
+      }
+    } else if (strcmp(argv[i], "-r") == 0) {
+      real_time = 1;
+    } else if (strcmp(argv[i], "-dp") == 0) {
+      print_lines = 0;
+    } else if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--stats") == 0) {
+      show_stats = 1;
+    }
+    /*
+    else if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--date") == 0)
     {
-        if (strcmp(argv[i], "-i") == 0 && i + 1 < argc)
+        if (i + 1 < argc)
         {
-            file_name = argv[++i];
-        }
-        else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
-        {
-            print_usage(argv[0]);
-            return EXIT_SUCCESS;
-        }
-        else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc)
-        {
-            if (filter_count < MAX_FILTERS)
+            start_date = normalize_date(argv[++i]);
+            if (start_date == NULL)
             {
-                filter_levels[filter_count++] = argv[++i];
+                fprintf(stderr, "Invalid start date format: %s\n", argv[i]);
+                return EXIT_FAILURE;
             }
-        }
-        else if (strcmp(argv[i], "-r") == 0)
-        {
-            real_time = 1;
-        }
-        else if (strcmp(argv[i], "-dp") == 0)
-        {
-            print_lines = 0;
-        }
-        else if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--stats") == 0)
-        {
-            show_stats = 1;
-        }
-        /*
-        else if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--date") == 0)
-        {
-            if (i + 1 < argc)
+            if (i + 1 < argc && argv[i + 1][0] != '-')
             {
-                start_date = normalize_date(argv[++i]);
-                if (start_date == NULL)
+                end_date = normalize_date(argv[++i]);
+                if (end_date != NULL)
                 {
-                    fprintf(stderr, "Invalid start date format: %s\n", argv[i]);
+                }
+                else
+                {
+                    fprintf(stderr, "Invalid end date format: %s\n", argv[i]);
                     return EXIT_FAILURE;
                 }
-                if (i + 1 < argc && argv[i + 1][0] != '-')
-                {
-                    end_date = normalize_date(argv[++i]);
-                    if (end_date != NULL)
-                    {
-                    }
-                    else
-                    {
-                        fprintf(stderr, "Invalid end date format: %s\n", argv[i]);
-                        return EXIT_FAILURE;
-                    }
-                }
             }
         }
-        */
     }
+    */
+  }
 
-    if (!file_name)
-    {
-        print_usage(argv[0]);
-        return EXIT_FAILURE;
-    }
+  if (!file_name) {
+    print_usage(argv[0]);
+    return EXIT_FAILURE;
+  }
 
-    start_log_monitor(file_name, filter_levels, filter_count, real_time, show_stats, print_lines, start_date, end_date);
+  start_log_monitor(file_name, filter_levels, filter_count, real_time,
+                    show_stats, print_lines, start_date, end_date);
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
